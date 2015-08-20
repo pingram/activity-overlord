eb77ffcedbb3d91777e5f105ce713df2bfdd073f
Philip Ingram <philip.d.ingram@gmail.com>
2015-08-20 10:11:03 -0500

    add index, edit, update, and destroy

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -52,7 +52,7 @@ module.exports = {
   edit: function (req, res, next) {
     User.findOne(req.param('id'), function foundUser (err, user) {
       if (err) return next(err);
-      if (!user) return next();
+      if (!user) return next("User doesn't exist.");
 
       res.view({
         user: user
@@ -67,6 +67,20 @@ module.exports = {
       }
 
       res.redirect('/user/show/' + req.param('id'));
+    });
+  },
+
+  destroy: function (req, res, next) {
+    User.findOne(req.param('id'), function foundUser (err, user) {
+      if (err) return next(err);
+
+      if (!user) return next("User doesn't exist.");
+
+      User.destroy(req.param('id'), function userDestroyed (err) {
+        if (err) return next(err);
+      });
+
+      res.redirect('/user');
     });
   }
 
